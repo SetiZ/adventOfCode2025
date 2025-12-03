1d8b32051ab61a0c46704f87964e2a0bcc7942c2
SetiZ <funkysetiz@gmail.com>
2025-12-03 09:56:36 +0100

    rename

--- day3/part1.js
+++ day3/part1.js
@@ -1,4 +1,11 @@
 import fs from "fs";
 
 const ex = fs.readFileSync("example").toString().replace(/\r|\n/g, "");
-const array = fs.readFileSync("input1").toString().replace(/\r|\n/g, "");
+const array = fs.readFileSync("input").toString().replace(/\r|\n/g, "");
+
+console.log(ex);
+
+for (let i = 0; i < ex.length; i++) {
+  const split = ex[i].split();
+  console.log(split);
+}
